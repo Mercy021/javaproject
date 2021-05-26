68e74e56aa777e9f424f0bbe974825a29bb3796b
mercy021 <kinyanjuimercywangari@gmail.com>
2021-05-26 14:01:58 +0300

    first commit

---
+++ ject.js
@@ -0,0 +1,13 @@
+//object
+var musician={firstname:"Mercy",County:"Nairobi",Networth:"1 million",Age:"21"}
+console.log(musician);
+let a='I wanna bake a lemon cake ';
+console.log(a+='using vanilla essence,flour,eggs and juice')
+//strings
+var guitar="I love playing acoustic guitar and piano"
+console.log(guitar);var x = 10;
+//booleans
+var b = 45;
+var c = 54;
+console.log(x == b);      
+console.log(x == c);
